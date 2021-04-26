953b8827ebc7997e4c8d63ed81db5bbd363e052f
Dewey C <deweycraddock@gmail.com>
2021-04-26 18:06:34 -0500

    Ineractive VR CSS update

--- src/Content.jsx
+++ src/Content.jsx
@@ -9,14 +9,17 @@ function Content() {
 
             <div className="content__info">
 
-                <div className="content__imgContainer">
-                    <img className='content__img' src={VRImage} alt="test" />
-                </div>
                 <div className="content__infoContainer">
-                    <h2 className="content__title">The leader in interactive VR</h2>
-                    <p className="content__info-content">
-                        Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.
-                    </p>
+                    <div className="content__imgContainer">
+                        <img className='content__img' src={VRImage} alt="test" />
+                    </div>
+                    <div className="content__right-block">
+
+                        <h2 className="content__title">The leader in interactive VR</h2>
+                        <p className="content__info-content">
+                            Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.
+                        </p>
+                    </div>
                 </div>
 
                 <div className="content__showcase">
