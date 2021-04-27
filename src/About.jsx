4aec59553ca70ad9aa623eeff5f8ea59cbf5ec32
Dewey C <deweycraddock@gmail.com>
2021-04-27 09:22:49 -0500

    About Page

--- src/About.jsx
+++ src/About.jsx
@@ -9,7 +9,17 @@ function About() {
             <div className="nav__header">
                 <Nav />
             </div>
-            <h1>About Page</h1>
+            <div className="about__content">
+                {/* Title */}
+                <h1 className='about__title'>About Us</h1>
+                {/* Image */}
+                <img src={require("./images/About-us-img.jpg").default} alt="test" />
+
+                {/* Text */}
+                <p className='about__text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, impedit aperiam. Cum ratione mollitia laboriosam cumque ipsam eaque, blanditiis velit inventore praesentium accusamus, perferendis minima. Temporibus, aliquid libero? Doloribus optio iure ipsam quod dicta itaque numquam? Quasi natus facilis distinctio itaque illum at commodi alias facere nulla, nemo totam sint, sit blanditiis est unde.</p>
+            </div>
+
+
         </div>
     )
 }
